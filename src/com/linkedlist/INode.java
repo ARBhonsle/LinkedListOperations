deff9988fe2df874deca77271dc80efeb3846348
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-20 17:51:35 +0530

    Added: Comments added

--- src/com/linkedlist/INode.java
+++ src/com/linkedlist/INode.java
@@ -5,11 +5,12 @@ package com.linkedlist;
  * @param <K>
  */
 public interface INode<K> {
+    // method to get variable key value
     K getKey();
-
+    // method to set variable key value
     void setKey(K key);
-
+    // method to get next node
     INode<K> getNext();
-
+    // method to set next node
     void setNext(INode<K> next);
 }
--- src/com/linkedlist/Node.java
+++ src/com/linkedlist/Node.java
@@ -5,31 +5,32 @@ package com.linkedlist;
  * @param <K>
  */
 public class Node<K> implements INode<K> {
+    // variable
     private K key;
     private INode<K> next;
-
+    // constructor
     public Node(K key) {
         this.key = key;
     }
-
+    // method to get key value
     @Override
     public K getKey() {
         return key;
     }
-
+    // method to set key value
     @Override
     public void setKey(K key) {
         this.key = key;
     }
-
+    // method to get next node
     public INode<K> getNext() {
         return next;
     }
-
+    // method to set next node
     public void setNext(INode<K> next) {
         this.next = next;
     }
-
+    // method to display variable key value and next node if exist
     public String printString() {
         StringBuilder myNodeString = new StringBuilder();
         myNodeString.append("MyNode{" + "key=").append(key).append("}");
