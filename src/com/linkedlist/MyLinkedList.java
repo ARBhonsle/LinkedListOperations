7682cbbc9258e39dc48e13e33d363ac606d459be
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-20 18:42:04 +0530

    Added: Method to add nodes to linked list

--- src/com/linkedlist/MyLinkedList.java
+++ src/com/linkedlist/MyLinkedList.java
@@ -13,15 +13,32 @@ public class MyLinkedList<K> {
         this.head = null;
         this.tail = null;
     }
-
+    public void addNode(INode<K> myNode){
+        if(this.tail==null){
+            this.tail=myNode;
+        }
+        if(this.head==null){
+            this.head=myNode;
+        }else{
+            INode<K> tempNode=this.head;
+            this.head=myNode;
+            this.head.setNext(tempNode);
+        }
+    }
     public static <K> void main(String[] args) {
         //Welcome to LinkedList
         System.out.println("Linked List Implementation");
+        // INode interface reference to object of Node class
         INode<Integer> nodeOne = new Node<>(56);
         INode<Integer> nodeTwo = new Node<>(30);
         INode<Integer> nodeThree = new Node<>(70);
-        nodeOne.setNext(nodeTwo);
-        nodeTwo.setNext(nodeThree);
+        // MyLinkedList object created
+        MyLinkedList linkedList=new MyLinkedList();
+        // addNode adds nodes to linked list
+        linkedList.addNode(nodeThree);
+        linkedList.addNode(nodeTwo);
+        linkedList.addNode(nodeOne);
+        // displays list values
         System.out.println(nodeOne.displayKey() + nodeTwo.displayKey() + nodeThree.displayKey());
     }
 }
