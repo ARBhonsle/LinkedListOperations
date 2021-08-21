b1145768d2c54957acf0ad51187acc6bf9ff4f4c
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-21 20:55:45 +0530

    Refactor : Stack operation message displayed

--- src/com/stack/MyStack.java
+++ src/com/stack/MyStack.java
@@ -1,4 +1,5 @@
 package com.stack;
+
 import com.linkedlist.INode;
 import com.linkedlist.MyLinkedList;
 import com.linkedlist.Node;
@@ -9,30 +10,34 @@ import com.linkedlist.Node;
 public class MyStack {
     MyLinkedList linkedList;
 
-    public MyStack(){
-        linkedList=new MyLinkedList();
+    public MyStack() {
+        linkedList = new MyLinkedList();
     }
 
-    public INode<Integer> createNode(Integer key){
+    public INode<Integer> createNode(Integer key) {
         return new Node<>(key);
     }
 
-    public void push(Integer key){
+    public void push(Integer key) {
         linkedList.addNode(createNode(key));
     }
-    public void display(){
+
+    public void display() {
         linkedList.displayLinkedList();
     }
-    public INode<Integer> peek(){
+
+    public INode<Integer> peek() {
         return linkedList.head;
     }
 
-    public void pop(){
+    public void pop() {
         linkedList.pop();
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         // MyStack object created
-        MyStack stack=new MyStack();
+        System.out.println("Stack Operations");
+        MyStack stack = new MyStack();
         // elements pushed to stack
         System.out.println("56 pushed to stack");
         stack.push(56);
@@ -42,7 +47,7 @@ public class MyStack {
         stack.push(70);
         System.out.println("Stack displayed");
         stack.display();
-        System.out.println("Top Node : "+stack.peek().getKey());
+        System.out.println("Top Node : " + stack.peek().getKey());
         System.out.println("Pop stack elements: 70, 30, 56");
         stack.pop();
         stack.display();
